1651224e043b077969b3949c9db1aa73579e4db2
Eric Loudon <loudon.eric@gmail.com>
2019-08-16 12:41:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,6 +57,11 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+holiday_hash.each do |season, holiday|
+  puts "#{season.capitalize}:"
+  holiday_hash.each do |holiday, item|
+    puts "#{}{holiday.to_s.split(" ").map {i |i|.capitalize} }
+    .join(" "):} #{item.join(", ")}"
 
 end
 
